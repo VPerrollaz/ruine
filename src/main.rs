026e8051c52fd7619ed7bdbb5013124e4d4efc3f
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2018-10-28 18:54:35 +0100

    Code de l'exécution parallèle manuelle mise dans une fonction propre.

--- src/main.rs
+++ src/main.rs
@@ -16,45 +16,55 @@ fn main() {
          proba = 0.5f64;
     }
 
-    let mut children = vec![];
-    for depart in 0..4 {
-        children.push(std::thread::spawn(move || { let mut resultat = vec![];
-            for i in 0..25 {
-                resultat.push(estimation_proba(40000u32, (i+25*depart) as i8, proba));
-            }
-            resultat
-        }));
-    }
+    let resultats = parallele(100i16, 1000u32, proba);
 
-    let mut resultats = [0.0f64; 100];
-    for (i, child) in children.into_iter().enumerate() {
-        for (j, val) in child.join().unwrap().iter().enumerate() {
-            resultats[25*i+j] = *val;
-        }
-    }
-    affichage(resultats);
+    affichage(&resultats);
 }
 
-fn affichage(tableau: [f64; 100]) {
+fn affichage(tableau: &[f64]) {
     for val in tableau.iter() {
         print!("{}, ", val);
     }
 }
 
-fn une_partie(x: i8, d: &Bernoulli, gen: &mut rand::ThreadRng) -> bool {
-    let mut etat = x;
-    while (0 < etat) & (etat < 100) {
-        etat += 2 * (d.sample(gen) as i8) - 1;
+fn parallele(f_max: i16, nb_parties: u32, proba: f64) -> Vec<f64> {
+    let mut children = vec![];
+    for depart in 0..4 {
+        children.push(std::thread::spawn(move || { let mut resultat = vec![];
+            for i in 0..25 {
+                resultat.push(estimation_proba((i+25*depart) as i16,
+                                               f_max,
+                                               nb_parties,
+                                               proba));
+            }
+            resultat
+        }));
     }
-        etat==100
+
+    let mut resultats = vec![0.0f64; f_max as usize];
+    for (i, child) in children.into_iter().enumerate() {
+        for (j, val) in child.join().unwrap().iter().enumerate() {
+            resultats[25*i+j] = *val;
+        }
+    }
+
+    resultats
 }
 
-fn estimation_proba(nb_parties: u32, x: i8, proba: f64) -> f64 {
+fn une_partie(f_ini: i16, f_max:i16, d: &Bernoulli, gen: &mut rand::ThreadRng) -> bool {
+    let mut etat = f_ini;
+    while (0 < etat) & (etat < f_max) {
+        etat += 2 * (d.sample(gen) as i16) - 1;
+    }
+        etat==f_max
+}
+
+fn estimation_proba(f_ini: i16, f_max: i16, nb_parties: u32, proba: f64) -> f64 {
     let d = Bernoulli::new(proba);
     let mut gen = rand::thread_rng();
     let mut nb_victoires = 0u16;
     for _ in 0..nb_parties {
-        if une_partie(x, &d, &mut gen) {
+        if une_partie(f_ini, f_max, &d, &mut gen) {
             nb_victoires += 1;
         }
     }
