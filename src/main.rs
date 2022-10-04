696f4693e8334a7ae632d827bf57d96d626df045
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2022-10-04 20:10:47 +0200

    Ajuste le formattage

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,21 @@ use rayon::prelude::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Resultat {
-    f_max : i16,
-    nb_parties : u32,
-    proba : f64,
-    seq : bool,
-    valeurs:  Vec<f64>
+    f_max: i16,
+    nb_parties: u32,
+    proba: f64,
+    seq: bool,
+    valeurs: Vec<f64>,
 }
 
 impl Resultat {
     fn new(f_max: i16, nb_parties: u32, proba: f64, seq: bool, valeurs: Vec<f64>) -> Resultat {
         Resultat {
-            f_max : f_max,
-            nb_parties : nb_parties,
-            proba : proba,
-            seq : seq,
-            valeurs : valeurs
+            f_max,
+            nb_parties,
+            proba,
+            seq,
+            valeurs,
         }
     }
 }
@@ -46,37 +46,53 @@ fn main() {
                             .get_matches();
 
     let proba: f64 = matches.value_of("proba").unwrap_or("0.5").parse().unwrap();
-    let nb_parties: u32 = matches.value_of("nb_parties").unwrap_or("100").parse().unwrap();
+    let nb_parties: u32 = matches
+        .value_of("nb_parties")
+        .unwrap_or("100")
+        .parse()
+        .unwrap();
     let f_max: i16 = matches.value_of("f_max").unwrap_or("10").parse().unwrap();
 
     if matches.is_present("seq") {
-        let res = Resultat::new(f_max, nb_parties, proba, true, sequentiel(f_max, nb_parties, proba));
+        let res = Resultat::new(
+            f_max,
+            nb_parties,
+            proba,
+            true,
+            sequentiel(f_max, nb_parties, proba),
+        );
         println!("{}", serde_json::to_string(&res).unwrap());
-    }
-    else {
-        let res = Resultat::new(f_max, nb_parties, proba, false, parallele(f_max, nb_parties, proba));
+    } else {
+        let res = Resultat::new(
+            f_max,
+            nb_parties,
+            proba,
+            false,
+            parallele(f_max, nb_parties, proba),
+        );
         println!("{}", serde_json::to_string(&res).unwrap());
     }
 }
 
 fn sequentiel(f_max: i16, nb_parties: u32, proba: f64) -> Vec<f64> {
-    (1..f_max as usize).into_iter()
-              .map( |f_ini| {estimation_proba(f_ini as i16, f_max, nb_parties, proba)} )
-              .collect()
+    (1..f_max as usize)
+        .map(|f_ini| estimation_proba(f_ini as i16, f_max, nb_parties, proba))
+        .collect()
 }
 
 fn parallele(f_max: i16, nb_parties: u32, proba: f64) -> Vec<f64> {
-    (1..f_max as usize).into_par_iter()
-              .map( |f_ini| {estimation_proba(f_ini as i16, f_max, nb_parties, proba)} )
-              .collect()
+    (1..f_max as usize)
+        .into_par_iter()
+        .map(|f_ini| estimation_proba(f_ini as i16, f_max, nb_parties, proba))
+        .collect()
 }
 
-fn une_partie(f_ini: i16, f_max:i16, d: &Bernoulli, gen: &mut rand::ThreadRng) -> bool {
+fn une_partie(f_ini: i16, f_max: i16, d: &Bernoulli, gen: &mut rand::ThreadRng) -> bool {
     let mut etat = f_ini;
     while (0 < etat) & (etat < f_max) {
         etat += 2 * (d.sample(gen) as i16) - 1;
     }
-        etat==f_max
+    etat == f_max
 }
 
 fn estimation_proba(f_ini: i16, f_max: i16, nb_parties: u32, proba: f64) -> f64 {
